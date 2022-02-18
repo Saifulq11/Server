8e6d1a82652c5a807555e4bcdd2c7eec7c4ad71e
Saifulq11 <99237536+Saifulq11@users.noreply.github.com>
2022-02-18 14:33:23 +0800

    Add files via upload

---
+++ Scrapy.py
@@ -0,0 +1,14 @@
+import scrapy
+
+class NewSpider(scrapy.Spider):
+   name = "new_spider"
+   start_urls = ["https://brickset.com/sets/year-2012"]
+
+   def parse(self, response):
+      css_selector = 'img'
+      for x in response.css(css_selector):
+         newsel = '@src'
+         yield {
+                 'Image Link': x.xpath(newsel).extract_first(),
+         }
+
